1798638ba2480755b6b3e212b0f145a4c40a6dfd
Viktoria Björklund <viktoriabjorklund99@gmail.com>
2025-04-04 16:42:26 +0100

    added parameters

---
+++ frontend/src/components/CommentSection.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const CommentSection = () => {
+  return (
+    <div>
+        hello
+    </div>
+);
+};
+
+export default CommentSection;
